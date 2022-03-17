35e5e7da0f7cdcee1374417214b9ef37ce81688f
Antulio de Oliveira - SUPSE/SESEP/SECSD <ao@cin.ufpe.br>
2022-03-17 18:01:21 -0300

    Adicionando meu primeiro envio aluroni

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,10 @@
+import  Style  from "./Cardapio.module.scss";
+export default function Cardapio() {
+    return(
+        <main>
+            <nav className="menu">
+
+            </nav>
+        </main>
+    )
+}
